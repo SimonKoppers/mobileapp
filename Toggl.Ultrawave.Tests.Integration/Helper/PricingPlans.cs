a5760ae3bf81a285e2b0830d332b0531b8200af1
Simon Rozsival <simon@rozsival.com>
2017-09-21 10:04:28 +0200

    :white_check_mark: Add helper functions for setting the subscription

---
+++ Toggl.Ultrawave.Tests.Integration/Helper/PricingPlans.cs
@@ -0,0 +1,10 @@
+﻿namespace Toggl.Ultrawave.Tests.Integration
+{
+    public enum PricingPlans
+    {
+        Free = 0,
+        Starter = 101,
+        Premium = 103,
+        Enterprise = 16
+    }
+}
--- Toggl.Ultrawave.Tests.Integration/Helper/WorkspaceHelper.cs
+++ Toggl.Ultrawave.Tests.Integration/Helper/WorkspaceHelper.cs
@@ -7,6 +7,7 @@ using Toggl.Multivac.Models;
 using Toggl.Ultrawave.Models;
 using Toggl.Ultrawave.Network;
 using Toggl.Ultrawave.Serialization;
+using Task = System.Threading.Tasks.Task;
 
 namespace Toggl.Ultrawave.Tests.Integration.Helper
 {
@@ -14,22 +15,34 @@ namespace Toggl.Ultrawave.Tests.Integration.Helper
     {
         public static async Task<Workspace> CreateFor(IUser user)
         {
-            ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
-
             var newWorkspaceName = $"{Guid.NewGuid()}";
             var json = $"{{\"name\": \"{newWorkspaceName}\"}}";
 
+            var responseBody = await makeRequest("https://toggl.space/api/v9/workspaces", HttpMethod.Post, user, json);
+
+            var jsonSerializer = new JsonSerializer();
+            return jsonSerializer.Deserialize<Workspace>(responseBody);
+        }
+
+        public static async Task SetSubscription(IUser user, long workspaceId, PricingPlans plan)
+        {
+            var json = $"{{\"pricing_plan_id\": \"{(int)plan}\"}}";
+
+            await makeRequest($"https://toggl.space/api/v9/workspaces/{workspaceId}/subscriptions", HttpMethod.Post, user, json);
+        }
+
+        private static async Task<string> makeRequest(string endpoint, HttpMethod method, IUser user, string json)
+        {
+            ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+
             var requestMessage = AuthorizedRequestBuilder.CreateRequest(
-                Credentials.WithApiToken(user.ApiToken), "https://toggl.space/api/v9/workspaces", HttpMethod.Post);
+                Credentials.WithApiToken(user.ApiToken), endpoint, method);
             requestMessage.Content = new StringContent(json, Encoding.UTF8, "application/json");
 
             using (var client = new HttpClient())
             {
                 var response = await client.SendAsync(requestMessage);
-                var responseBody = await response.Content.ReadAsStringAsync();
-
-                var jsonSerializer = new JsonSerializer();
-                return jsonSerializer.Deserialize<Workspace>(responseBody);
+                return await response.Content.ReadAsStringAsync();
             }
         }
     }
