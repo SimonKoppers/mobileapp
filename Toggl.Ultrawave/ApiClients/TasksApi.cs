8019adc63182d7c2455ee81fcacdee3a71dc82d9
Simon Rozsival <simon@rozsival.com>
2017-09-21 10:05:22 +0200

    :sparkles: Add tasks API

--- Toggl.Ultrawave/ApiClients/TasksApi.cs
+++ Toggl.Ultrawave/ApiClients/TasksApi.cs
@@ -1,6 +1,34 @@
+using System;
+using System.Collections.Generic;
+using Toggl.Multivac.Models;
+using Toggl.Ultrawave.Models;
+using Toggl.Ultrawave.Network;
+using Toggl.Ultrawave.Serialization;
+
 namespace Toggl.Ultrawave.ApiClients
 {
-    internal sealed class TasksApi : ITasksApi
+    internal sealed class TasksApi : BaseApi, ITasksApi
     {
+        private readonly TaskEndpoints endPoints;
+
+        public TasksApi(TaskEndpoints endPoints, IApiClient apiClient, IJsonSerializer serializer, Credentials credentials)
+            : base(apiClient, serializer, credentials)
+        {
+            this.endPoints = endPoints;
+        }
+
+        public IObservable<List<ITask>> GetAll()
+            => CreateListObservable<Task, ITask>(endPoints.Get, AuthHeader);
+
+        public IObservable<List<ITask>> GetAllSince(DateTimeOffset threshold)
+            => CreateListObservable<Task, ITask>(endPoints.GetSince(threshold), AuthHeader);
+
+        public IObservable<ITask> Create(ITask task)
+        {
+            var endPoint = endPoints.Post(task.WorkspaceId, task.ProjectId);
+            var taskCopy = task as Task ?? new Task(task);
+            var observable = CreateObservable(endPoint, AuthHeader, taskCopy, SerializationReason.Post);
+            return observable;
+        }
     }
 }
