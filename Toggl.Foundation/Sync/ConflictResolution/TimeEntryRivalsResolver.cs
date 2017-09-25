9c602f8dd3e4e9c19787cb190afceb59ca2c3f0b
simonrozsival <simon@rozsival.com>
2017-09-25 17:12:30 +0200

    :sparkles: Add separate rivals resolver interface

---
+++ Toggl.Foundation/Sync/ConflictResolution/TimeEntryRivalsResolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Toggl.Foundation.Models;
+using Toggl.PrimeRadiant.Models;
+
+namespace Toggl.Foundation.Sync.ConflictResolution
+{
+    internal sealed class TimeEntryRivalsResolver : IRivalsResolver<IDatabaseTimeEntry>
+    {
+        public bool CanHaveRival(IDatabaseTimeEntry entity) => !entity.Stop.HasValue;
+
+        public Expression<Func<IDatabaseTimeEntry, bool>> AreRivals(IDatabaseTimeEntry entity)
+            => potentialRival => !entity.Stop.HasValue && !potentialRival.Stop.HasValue;
+
+        public (IDatabaseTimeEntry FixedEntity, IDatabaseTimeEntry FixedRival) FixRivals(IDatabaseTimeEntry entity, IDatabaseTimeEntry rival, IQueryable<IDatabaseTimeEntry> allTimeEntries)
+            => entity.At > rival.At ? (entity, stop(rival, allTimeEntries)) : (stop(entity, allTimeEntries), rival);
+
+        private IDatabaseTimeEntry stop(IDatabaseTimeEntry toBeStopped, IQueryable<IDatabaseTimeEntry> allTimeEntries)
+        {
+            var next = allTimeEntries
+                .OrderBy(te => te.Stop)
+                .FirstOrDefault(other => other.Stop > toBeStopped.Start);
+            var stop = next?.Start ?? DateTimeOffset.Now;
+            return new TimeEntry(toBeStopped, stop);
+        }
+    }
+}
