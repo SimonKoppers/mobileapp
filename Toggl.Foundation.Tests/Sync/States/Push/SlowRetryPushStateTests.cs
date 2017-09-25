3c101c1e9e7c308d2cbddaf7aec6ed91c8add5d6
simonrozsival <simon@rozsival.com>
2017-09-25 15:01:37 +0200

    :white_check_mark: Add tests for the new states

---
+++ Toggl.Foundation.Tests/Sync/States/Push/SlowRetryPushStateTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using Toggl.Foundation.Sync.States.Push;
+using Toggl.Ultrawave;
+
+namespace Toggl.Foundation.Tests.Sync.States.Push
+{
+    public sealed class SlowRetryPushStateTests : BaseRetryPushStateTests
+    {
+        protected override BaseRetryPushState<TestModel> CreateState(ITogglApi api, IScheduler scheduler, Random rnd)
+            => new SlowRetryPushState<TestModel>(api, scheduler, rnd);
+
+        protected override double DefaultWaitingTime => 60;
+        protected override double MinProlongingFactor => 1.5;
+        protected override double MaxProlongingFactor => 2;
+    }
+}
