8dddb3f1df68891095580e32777dff9cea038203
simonrozsival <simon@rozsival.com>
2017-09-25 17:30:57 +0200

    :sparkles: Use rivals resolver in persisting time entries

--- Toggl.Foundation/Sync/States/Pull/PersistTimeEntriesState.cs
+++ Toggl.Foundation/Sync/States/Pull/PersistTimeEntriesState.cs
@@ -12,7 +12,7 @@ namespace Toggl.Foundation.Sync.States
     class PersistTimeEntriesState : BasePersistState<ITimeEntry, IDatabaseTimeEntry>
     {
         public PersistTimeEntriesState(IRepository<IDatabaseTimeEntry> repository, ISinceParameterRepository sinceParameterRepository)
-            : base(repository, sinceParameterRepository, Resolver.ForTimeEntries())
+            : base(repository, sinceParameterRepository, Resolver.ForTimeEntries(), new TimeEntryRivalsResolver())
         {
         }
 
